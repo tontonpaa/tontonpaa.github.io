441d49f2cabae3380637193e90dbdf8700aa6026
tontonpaa <nktk.20202004@gmail.com>
2025-04-10 18:49:29 +0900

    intentsの明示化

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ load_dotenv()
 TOKEN = os.environ['DISCORD_TOKEN']
 
 intents = discord.Intents.default()
+intents.message_content = True  # メッセージの内容へのアクセスを有効にする
 # intents.threads = True
 # intents.guilds = True
 client = discord.Client(intents=intents)
