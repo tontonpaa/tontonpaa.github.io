2262843451e6d74aeb99099478874cf74d210eca
tontonpaa <nktk.20202004@gmail.com>
2025-05-01 13:20:09 +0900

    message.poll.answers

--- main.py
+++ main.py
@@ -182,7 +182,7 @@ async def on_message(message: discord.Message):
     # 投票メッセージの検知とスレッド作成
     if isinstance(message.channel, discord.TextChannel):
         if message.poll:
-            thread_name = message.poll.question[:100].strip()
+            thread_name = message.poll.answers[:100].strip()
 
             # 全角スペース（例：「タイトル　詳細」形式）で切り分け
             fullwidth_space_match = re.search(r'　', thread_name)
